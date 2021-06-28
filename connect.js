09aec5a95966b4052ce96e0b72925ce4b274216d
Chris Sev <chris.sevilleja@gmail.com>
2021-06-28 14:44:30 -0700

    wrk pls

--- connect.js
+++ connect.js
@@ -1,15 +1,19 @@
 const mongoose = require("mongoose");
 const fs = require("fs");
 
-let uri = process.env.MONGODB_DB;
-let cert = process.env.CA_CERT;
+let databaseUrl = process.env.DATABASE_URL;
+let caCert = process.env.CA_CERT;
+
+if (!databaseUrl) throw new Error("Please enter a DATABASE_URL");
+if (!caCert) throw new Error("Please enter a CA_CERT");
+
 let cachedClient;
 
 module.exports = async function connect() {
   if (cachedClient) return { client: cachedClient };
 
-  fs.writeFileSync("./ca-certificate.crt", cert);
-  const client = await mongoose.connect(uri, {
+  fs.writeFileSync("./ca-certificate.crt", caCert);
+  const client = await mongoose.connect(databaseUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     tlsCAFile: "./ca-certificate.crt",
--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 require("dotenv").config();
 
 const express = require("express");
+const connect = require("./connect");
 const app = express();
 const port = process.env.PORT || 3000;
 
-let databaseUrl = process.env.DATABASE_URL;
-let caCert = process.env.CA_CERT;
-
-if (!databaseUrl) throw new Error("Please enter a DATABASE_URL");
-if (!caCert) throw new Error("Please enter a CA_CERT");
-
-console.log(databaseUrl);
-console.log(caCert);
-
 app.get("/", (req, res) => {
+  const { client } = connect();
   res.send("Hello World!");
 });
 
